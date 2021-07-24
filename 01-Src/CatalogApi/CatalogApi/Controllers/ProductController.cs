385ba98225e7ba55f8122bb73cb9fb127586d2fa
Antony CANUT <canut42@gmail.com>
2021-07-24 11:35:31 -0700

    Initiate folders and project

---
+++ 01-Src/CatalogApi/CatalogApi/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
