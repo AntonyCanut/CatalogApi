b5e3008c5b3d93b6c518b660c178adf11c36cc43
Antony CANUT <canut42@gmail.com>
2021-07-24 12:18:36 -0700

    Create Model Base for Products

---
+++ 01-Src/CatalogApi/CatalogApi/Models/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogApi.Models
+{
+    public class Product
+    {
+        private string _code;
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private DateTime _startDate;
+
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        private DateTime _endDate;
+
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set { _endDate = value; }
+        }
+    }
+}
